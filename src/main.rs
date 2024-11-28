2af3c0070724fa6c5b973a1dc71b1ef1e698a72d
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2024-11-28 23:23:46 +0000

    Using modularization in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,9 @@
-use std::ptr::null;
+mod basics;
 
 fn main() {
-    let x = 5 + 5;
-    println!("Hello, world!. Result: {}", x);
-
-    println!("Base 10: {}", 69420);
-    println!("Base 2 (binary) {:b}", 69420);
-    println!("Base 8 (octal) {:o}", 69420);
-    println!("Base 16 (hexdecimal) {:x}", 69420);
-
-    // You can right-justify text with a specified width. This will
-    // output "    1". (Four white spaces and a "1", for a total width of 5.)
-    println!("{number:>1}", number=1);
-
-    let _logical: bool = true;
-    let _a_float: f32 = 1.0;
-    let _an_integer = 5f32;
-
-    let _default_float = 3.0; // default
-    let _default_integer = 7;
-
-    let mut inferred_type = 11;
-    inferred_type = 12;
-    println!("inferred_type: {}", inferred_type);
-
-    let my_array: [i64; 6] = [10, 20, 30, 40, 50, 0];
-    for element in my_array.iter() {
-        println!("element: {}", element);
-    }
-
-    let MyTuple = (5u32, 1u8, "hello", true, 'c', -5.04f32);
-    println!("{:?}", MyTuple);
-
+    // In Rust, the pub(crate) visibility modifier allows the item to be accessible within the same crate.
+    // This is necessary because main.rs and basics_01_data_types.rs are part of the same crate,
+    // and you want to expose the data_types_crate function to other modules within that crate.
+    basics::basics_01_data_types::data_types_crate();
+    basics::basics_01_data_types::basics::another_function_usage();
 }
