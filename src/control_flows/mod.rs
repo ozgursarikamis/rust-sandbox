480736ec96b078007e85752b986e88ac0b62b6e6
Ozgur Sarikamis <ozgursarikamis@gmail.com>
2024-11-30 02:14:33 +0000

    Functions: Looping

--- src/control_flows/mod.rs
+++ src/control_flows/mod.rs
@@ -11,3 +11,17 @@ pub(crate) fn return_conditionally() -> i32 {
     let number = if condition { 5 } else { 6 };
     number
 }
+
+pub(crate) fn looping() {
+    let mut counter = 0;
+    let result = loop {
+        counter += 1;
+        println!("\t {}", counter);
+
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {}", result);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,9 @@
-use crate::control_flows::{
-    if_else_condition,
-    return_conditionally
-};
+use crate::control_flows::{if_else_condition, looping, return_conditionally};
 
 mod basics;
 mod control_flows;
-mod functions;
+// mod functions;
 
 fn main() {
-    if_else_condition(4);
-    if_else_condition(6);
-
-    println!("returned number is: {:?}", return_conditionally());
+    looping();
 }
